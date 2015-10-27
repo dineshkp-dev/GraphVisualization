d816ce4992463cce4733c2472d255dd47885655a
dinesh <dingy007@gmail.com>
2015-10-27 16:48:51 +0800

    Commit updates to JSON load.

--- GraphVisualizationHelper/src/main/java/com/graphvisual/myApp/HomeController.java
+++ GraphVisualizationHelper/src/main/java/com/graphvisual/myApp/HomeController.java
@@ -8,15 +8,19 @@ import java.io.InputStream;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Scanner;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.FileSystemResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,6 +32,9 @@ public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
+	@Autowired
+	private ResourceLoader resourceLoader;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -46,19 +53,39 @@ public class HomeController {
 	}
 
 	@RequestMapping(value = "/filerequest", method = RequestMethod.GET)
-	public void miserables_request(	HttpServletResponse response) {
+	public void fileRequest(HttpServletRequest request,	HttpServletResponse response) throws IOException {
 		logger.info("Providing miserables.json file");
 
-//		File file = new File("C:\\Users\\dineshkp\\Desktop\\GitRepo_local\\GraphVisualization\\GraphVisualizationHelper\\src\\main\\resources\\miserables.json");
-//		File file = new File ("classpath:/miserables.json");
-		String fileName = "miserables.json";
-		ClassLoader classLoader = getClass().getClassLoader();
-		File file = new File(classLoader.getResource(fileName).getFile());
+		// get absolute path of the application
+		String appPath = request.getContextPath();
+		//        String appPath = context.getRealPath("");
+		logger.info("appPath = " + appPath);
+
+		String filePath = "classpath:/miserables.json";
+		//        String filePath = "/WEB-INF/resources/miserables.json";
+		// construct the complete absolute path of the file
+//		String fullPath =appPath + filePath;      
+
+		//		File file = new File("C:\\Users\\dineshkp\\Desktop\\GitRepo_local\\GraphVisualization\\GraphVisualizationHelper\\src\\main\\resources\\miserables.json");
+		//		File file = new File ("/resources/miserables.json");
+		//		String fileName = "classpath:";
+
+		//		FileSystemResource resource = new FileSystemResource(fullPath);	
+		//		FileSystemResource resource = new FileSystemResource("C:\\Users\\dineshkp\\Desktop\\GitRepo_local\\GraphVisualization\\GraphVisualizationHelper\\src\\main\\resources\\miserables.json");
+
+
+		//		File file = new File(fileName);
+		//		System.out.println(file.toString());
+
+//		Resource resource = resourceLoader.getResource("classpath:/miserables.json");
+		Resource resource = resourceLoader.getResource(filePath);
+		File file = resource.getFile();
 		try {
-			
-			InputStream is = new FileInputStream(file);
+
+			InputStream in = new FileInputStream(file);
+//						InputStream in = resource.getInputStream();
 			// copy it to response's OutputStream
-			org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+			org.apache.commons.io.IOUtils.copy(in, response.getOutputStream());
 
 			response.flushBuffer();
 		}
