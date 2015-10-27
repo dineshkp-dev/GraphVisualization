a3ab5682fcfdac07a8bb6e28942e25673fcf5857
dingy007 <dingy007@gmail.com>
2015-10-27 23:32:21 +0800

    Updating the JSON POJOs for Graph creation

--- GraphVisualizationHelper/src/main/java/com/graphvisual/myApp/HomeController.java
+++ GraphVisualizationHelper/src/main/java/com/graphvisual/myApp/HomeController.java
@@ -15,8 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
@@ -34,7 +32,7 @@ public class HomeController {
 
 	@Autowired
 	private ResourceLoader resourceLoader;
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -49,41 +47,23 @@ public class HomeController {
 
 		model.addAttribute("serverTime", formattedDate );
 
-		return "home.jsp";
+		return "home";
 	}
 
 	@RequestMapping(value = "/filerequest", method = RequestMethod.GET)
 	public void fileRequest(HttpServletRequest request,	HttpServletResponse response) throws IOException {
-		logger.info("Providing miserables.json file");
+		logger.info("Providing Json  file");
 
-		// get absolute path of the application
 		String appPath = request.getContextPath();
-		//        String appPath = context.getRealPath("");
 		logger.info("appPath = " + appPath);
 
-		String filePath = "classpath:/miserables.json";
-		//        String filePath = "/WEB-INF/resources/miserables.json";
-		// construct the complete absolute path of the file
-//		String fullPath =appPath + filePath;      
+		String filePath = "classpath:/test_graph.json";
 
-		//		File file = new File("C:\\Users\\dineshkp\\Desktop\\GitRepo_local\\GraphVisualization\\GraphVisualizationHelper\\src\\main\\resources\\miserables.json");
-		//		File file = new File ("/resources/miserables.json");
-		//		String fileName = "classpath:";
-
-		//		FileSystemResource resource = new FileSystemResource(fullPath);	
-		//		FileSystemResource resource = new FileSystemResource("C:\\Users\\dineshkp\\Desktop\\GitRepo_local\\GraphVisualization\\GraphVisualizationHelper\\src\\main\\resources\\miserables.json");
-
-
-		//		File file = new File(fileName);
-		//		System.out.println(file.toString());
-
-//		Resource resource = resourceLoader.getResource("classpath:/miserables.json");
 		Resource resource = resourceLoader.getResource(filePath);
 		File file = resource.getFile();
 		try {
 
 			InputStream in = new FileInputStream(file);
-//						InputStream in = resource.getInputStream();
 			// copy it to response's OutputStream
 			org.apache.commons.io.IOUtils.copy(in, response.getOutputStream());
 
@@ -94,7 +74,6 @@ public class HomeController {
 			throw new RuntimeException("IOError writing file to output stream");
 		}
 		catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		finally {
