53846fe902242a8d2d83e7d27aa53ed4722ea90e
dineshkp <Dinesh-Kumar.Pulikesi@amd.com>
2015-10-28 17:29:12 +0800

    Updating the POM FILE

--- GraphVisualizationHelper/src/main/java/com/graphvisual/myApp/jsonGen/Link.java
+++ GraphVisualizationHelper/src/main/java/com/graphvisual/myApp/jsonGen/Link.java
@@ -6,9 +6,9 @@ import java.util.Map;
 
 import javax.annotation.Generated;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Generated("org.jsonschema2pojo")
